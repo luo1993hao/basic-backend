4681e523d3acccb537f1c10100ff6e211214be30
HaoYu Luo <haoyu.luo@thoughtworks.com>
2020-09-10 12:59:03 +0800

    test

---
+++ src/main/java/com/basicbackend/BasicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.basicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basicbackend/BasicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
