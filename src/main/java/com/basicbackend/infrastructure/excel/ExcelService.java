3744095f0c65ed0d92643ffc83230b2c72fedfc2
luo1993hao <283858831@qq.com>
2020-09-16 16:51:58 +0800

    add excel support

---
+++ src/main/java/com/basicbackend/infrastructure/excel/ExcelService.java
@@ -0,0 +1,59 @@
+package com.basicbackend.infrastructure.excel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.basicbackend.infrastructure.excel.listener.DemoDataListener;
+import com.basicbackend.infrastructure.excel.model.DemoData;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * https://www.yuque.com/easyexcel/doc/easyexcel
+ */
+public class ExcelService {
+    public void read() {
+        // 写法1：
+        String fileName = "";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+
+    }
+
+    public void write() {
+        // 写法1
+        String fileName = "";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法2
+        fileName = "";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = null;
+        try {
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+            excelWriter.write(data(), writeSheet);
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
+    }
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/basicbackend/infrastructure/excel/model/DemoData.java
@@ -0,0 +1,12 @@
+package com.basicbackend.infrastructure.excel.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class DemoData {
+    private String string;
+    private Date date;
+    private Double doubleData;
+}
